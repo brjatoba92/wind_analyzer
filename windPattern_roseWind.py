ce0808e45be8555ad2acd38ffe9282d82b50635d
brjatoba92 <brjatoba92@gmail.com>
2025-06-17 16:12:08 -0300

    Instalação das dependencias, restrição da pasta .venv para não subir ao Github

---
+++ windPattern_roseWind.py
@@ -0,0 +1,9 @@
+# Dependencias
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib.projections import PolarAxes
+import mpl_toolkits.axisartist.grid_finder as gf
+import mpl_toolkits.axisartist.floating_axes as fa
+from scipy.stats import weibull_min
+from typing import Dict, List, Optional, Tuple
